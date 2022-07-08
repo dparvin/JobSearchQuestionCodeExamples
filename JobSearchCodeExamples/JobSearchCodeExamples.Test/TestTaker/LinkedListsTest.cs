ce78e8d01c31452b7666d3fa517fd89c040063b2
David Parvin <parvin_david@hotmail.com>
2022-07-08 13:57:30 -0600

    Added the code for a reversal of a linked list

---
+++ JobSearchCodeExamples/JobSearchCodeExamples.Test/TestTaker/LinkedListsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchCodeExamples.Test.TestTaker
+{
+    public class LinkedListsTest
+    {
+        /// <summary>
+        /// Tests the linked list reverse.
+        /// </summary>
+        [Fact]
+        public void TestLinkedListReverse()
+        {
+            LinkedList<string> list = new();
+            list.AddFirst("2");
+            list.AddLast("3");
+            list.AddLast("4");
+            list.AddLast("5");
+            var result = LinkedLists.ReverseList(list);
+            Assert.Equal(list.Last?.Value, result.First?.Value);
+        }
+
+        /// <summary>
+        /// Tests the linked list reverse.
+        /// </summary>
+        [Fact]
+        public void TestLinkedListNull()
+        {
+            LinkedList<string>? list = null;
+            var result = LinkedLists.ReverseList(list);
+            Assert.Null(result);
+        }
+
+        /// <summary>
+        /// Tests the linked list reverse.
+        /// </summary>
+        [Fact]
+        public void TestLinkedListEmpty()
+        {
+            LinkedList<string>? list = new();
+            var result = LinkedLists.ReverseList(list);
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ JobSearchCodeExamples/JobSearchCodeExamples/TestTaker/LinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace JobSearchCodeExamples.TestTaker
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class LinkedLists
+    {
+        /// <summary>
+        /// Reverses the list.
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <returns></returns>
+        public static LinkedList<T>? ReverseList<T>(LinkedList<T>? list)
+        {
+            LinkedList<T> results = new();
+            if (list == null || list.Count == 0) return list;
+            LinkedListNode<T>? currentItem = list.First;
+            while (currentItem != null)
+            {
+                results.AddFirst(currentItem.Value);
+                currentItem = currentItem.Next;
+            }
+
+            return results;
+        }
+    }
+}
