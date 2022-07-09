6b008b01536157a85d44f9e3f879ba16c613f4f8
David Parvin <parvin_david@hotmail.com>
2022-07-08 19:22:01 -0600

    Added code to get he max number of people who were employed at the same time.

---
+++ JobSearchCodeExamples/JobSearchCodeExamples.Test/TestTaker/EmployeeCoverageTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchCodeExamples.Test.TestTaker;
+
+/// <summary>
+/// Tests for Employee Coverage
+/// </summary>
+public class EmployeeCoverageTest
+{
+    /// <summary>
+    /// Maximums the coverage test.
+    /// </summary>
+    [Fact]
+    public void MaxCoverageTest()
+    {
+        string[] employees = {
+            "79-84",
+            "80-86",
+            "75-83",
+            "90-99",
+            "87-95" };
+        var results = EmployeeCoverage.MaxCoverage(employees);
+
+        Assert.Equal(3, results);
+    }
+}
---
+++ JobSearchCodeExamples/JobSearchCodeExamples/TestTaker/EmployeeCoverage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchCodeExamples.TestTaker;
+
+/// <summary>
+/// Routines to calculate employee coverage
+/// </summary>
+public static class EmployeeCoverage
+{
+    /// <summary>
+    /// Maximums the coverage.
+    /// </summary>
+    /// <param name="employees">The employees.</param>
+    /// <returns></returns>
+    public static int MaxCoverage(string[] employees)
+    {
+        int[] combined = new int[employees.Length];
+
+        for (int i = 0; i < employees.Length; i++)
+        {
+            string[] years = employees[i].Split("-");
+            int startDate = int.Parse(years[0]);
+            int endDate = int.Parse(years[1]);
+            for (int j = i + 1; j < employees.Length; j++)
+            {
+                string[] years2 = employees[j].Split("-");
+                int start = int.Parse(years2[0]);
+                int end = int.Parse(years2[1]);
+                if (startDate <= start && endDate >= start ||
+                    startDate <= end && endDate >= end ||
+                    startDate <= start && endDate >= end ||
+                    startDate >= start && endDate <= end)
+                {
+                    combined[i] += combined[i] == 0 ? 2 : 1;
+                    combined[j] += combined[j] == 0 ? 2 : 1;
+                }
+            }
+        }
+        var maxValue = 0;
+        for (int i = 0; i < employees.Length; i++)
+            if (combined[i] > maxValue) maxValue = combined[i];
+
+        return maxValue;
+    }
+}
