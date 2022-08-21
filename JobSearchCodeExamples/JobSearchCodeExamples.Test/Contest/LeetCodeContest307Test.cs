ae5d1c94e6ae4c909e6bf5e0190c8db0fe8cea51
David Parvin <parvin_david@hotmail.com>
2022-08-20 20:25:18 -0600

    Got the code ready for the next contest

---
+++ JobSearchCodeExamples/JobSearchCodeExamples.Test/Contest/LeetCodeContest307Test.cs
@@ -0,0 +1,59 @@
+﻿using JobSearchCodeExamples.Contest;
+
+namespace JobSearchCodeExamples.Test.Contest
+{
+    public class LeetCodeContest307Test
+    {
+        #region Question 1 ------------------------------------------
+
+        /// <summary>
+        /// Question 1.
+        /// </summary>
+        [Fact]
+        public void Question1()
+        {
+            LeetCodeContest307.Question1();
+        }
+
+        #endregion
+
+        #region Question 2 ------------------------------------------
+
+        /// <summary>
+        /// Question 2.
+        /// </summary>
+        [Fact]
+        public void Question2()
+        {
+            LeetCodeContest307.Question2();
+        }
+
+        #endregion
+
+        #region Question 3 ------------------------------------------
+
+        /// <summary>
+        /// Question 3.
+        /// </summary>
+        [Fact]
+        public void Question3()
+        {
+            LeetCodeContest307.Question3();
+        }
+
+        #endregion
+
+        #region Question 4 ------------------------------------------
+
+        /// <summary>
+        /// Question 4.
+        /// </summary>
+        [Fact]
+        public void Question4()
+        {
+            LeetCodeContest307.Question4();
+        }
+
+        #endregion
+    }
+}
---
+++ JobSearchCodeExamples/JobSearchCodeExamples/Contest/LeetCodeContest307.cs
@@ -0,0 +1,56 @@
+﻿namespace JobSearchCodeExamples.Contest
+{
+    /// <summary>
+    /// LeetCode Contest 307
+    /// </summary>
+    public static class LeetCodeContest307
+    {
+        #region Question 1 ------------------------------------------
+
+        /// <summary>
+        /// Question 1.
+        /// </summary>
+        public static void Question1()
+        {
+
+        }
+
+        #endregion
+
+        #region Question 2 ------------------------------------------
+
+        /// <summary>
+        /// Question 2.
+        /// </summary>
+        public static void Question2()
+        {
+
+        }
+
+        #endregion
+
+        #region Question 3 ------------------------------------------
+
+        /// <summary>
+        /// Question 3.
+        /// </summary>
+        public static void Question3()
+        {
+
+        }
+
+        #endregion
+
+        #region Question 4 ------------------------------------------
+
+        /// <summary>
+        /// Question 4.
+        /// </summary>
+        public static void Question4()
+        {
+
+        }
+
+        #endregion
+    }
+}
