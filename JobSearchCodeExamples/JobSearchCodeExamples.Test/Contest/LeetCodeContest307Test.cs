63fc4fc18f6a331789c50b9802e9345b1f17ab05
David Parvin <parvin_david@hotmail.com>
2022-08-27 19:40:14 -0600

    Added stuff to contest 307  Still not done

--- JobSearchCodeExamples/JobSearchCodeExamples.Test/Contest/LeetCodeContest307Test.cs
+++ JobSearchCodeExamples/JobSearchCodeExamples.Test/Contest/LeetCodeContest307Test.cs
@@ -1,57 +1,122 @@
 ﻿using JobSearchCodeExamples.Contest;
+using static JobSearchCodeExamples.Contest.LeetCodeContest307;
 
 namespace JobSearchCodeExamples.Test.Contest
 {
-    public class LeetCodeContest307Test
+    public partial class LeetCodeContest307Test
     {
-        #region Question 1 ------------------------------------------
+        #region Minimum Hours of Training to Win a Competition ------
 
         /// <summary>
-        /// Question 1.
+        /// Minimums the number of hours test.
         /// </summary>
-        [Fact]
-        public void Question1()
+        /// <param name="initialEnergy">The initial energy.</param>
+        /// <param name="initialExperience">The initial experience.</param>
+        /// <param name="energy">The energy.</param>
+        /// <param name="experience">The experience.</param>
+        /// <param name="expectedResult">The expected result.</param>
+        [Theory]
+        [InlineData(5, 3, new int[] { 1, 4, 3, 2 }, new int[] { 2, 6, 4, 1 }, 8)]
+        [InlineData(2, 4, new int[] { 1 }, new int[] { 3 }, 0)]
+        [InlineData(100, 100, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 1, 2, 3, 1, 2, 10 }, 0)]
+        public void MinNumberOfHoursTest(int initialEnergy, int initialExperience, int[] energy, int[] experience, int expectedResult)
         {
-            LeetCodeContest307.Question1();
+            Assert.Equal(expectedResult, LeetCodeContest307.MinNumberOfHours(initialEnergy, initialExperience, energy, experience));
         }
 
         #endregion
 
-        #region Question 2 ------------------------------------------
+        #region Largest Palindromic Number --------------------------
 
         /// <summary>
-        /// Question 2.
+        /// The largest palindromic test.
         /// </summary>
-        [Fact]
-        public void Question2()
+        /// <param name="num">The number.</param>
+        /// <param name="expectedResult">The expected result.</param>
+        [Theory]
+        [InlineData("444947137", "7449447")]
+        [InlineData("00009", "9")]
+        [InlineData("00001105", "1005001")]
+        [InlineData("00000", "0")]
+        public void LargestPalindromic1Test(string num, string expectedResult)
         {
-            LeetCodeContest307.Question2();
+            Assert.Equal(expectedResult, LeetCodeContest307.LargestPalindromic(num));
+        }
+
+        /// <summary>
+        /// The largest palindromic test.
+        /// </summary>
+        /// <param name="num">The number.</param>
+        /// <param name="expectedResult">The expected result.</param>
+        [Theory]
+        [InlineData("444947137", "7449447")]
+        [InlineData("00009", "9")]
+        [InlineData("00001105", "1005001")]
+        [InlineData("00000", "0")]
+        public void LargestPalindromic2Test(string num, string expectedResult)
+        {
+            Assert.Equal(expectedResult, LeetCodeContest307.LargestPalindromic2(num));
         }
 
         #endregion
 
-        #region Question 3 ------------------------------------------
+        #region Amount of Time for Binary Tree to Be Infected -------
 
         /// <summary>
-        /// Question 3.
+        /// Amounts the of time test.
         /// </summary>
-        [Fact]
-        public void Question3()
+        /// <param name="nodes">The nodes.</param>
+        /// <param name="start">The start.</param>
+        /// <param name="expectedResult">The expected result.</param>
+        [Theory]
+        [InlineData(new int[] { 1, 5, 3, 0, 4, 10, 6, 9, 2 }, 3, 4)]
+        [InlineData(new int[] { 1 }, 1, 0)]
+        public void AmountOfTimeTest(int[] nodes, int start, int expectedResult)
         {
-            LeetCodeContest307.Question3();
+            var root = new TreeNode(nodes[0]);
+            var Index = 1;
+            var pos = 1;
+            while (Index < nodes.Length)
+            {
+                AddBranchs(ref root, nodes, pos, ref Index);
+                pos++;
+            }
+
+            Assert.Equal(expectedResult, AmountOfTime(root, start));
+        }
+
+        private static void AddBranchs(ref TreeNode? parent, int[] nodes, int pos, ref int index, int currLevel = 0)
+        {
+            var posLevel = (int)Math.Pow(pos, .5);
+            var posLeft = Math.Round((pos - ((int)Math.Pow(2, posLevel)-1.0)) / (int)Math.Pow(posLevel, 2), 0);
+            if (parent == null && posLevel == currLevel && nodes[index] != 0)
+            {
+                parent = new TreeNode(nodes[index], pos);
+                index++;
+            }
+            else if (parent == null && posLevel == currLevel && nodes[index] == 0)
+                index++;
+            else if (parent != null && posLeft == 0)
+                AddBranchs(ref parent.left, nodes, pos, ref index, currLevel + 1);
+            else if (parent != null && posLeft == 1)
+                AddBranchs(ref parent.right, nodes, pos, ref index, currLevel + 1);
         }
 
         #endregion
 
-        #region Question 4 ------------------------------------------
+        #region Find the K-Sum of an Array --------------------------
 
         /// <summary>
-        /// Question 4.
+        /// k sum test.
         /// </summary>
-        [Fact]
-        public void Question4()
+        /// <param name="nums">The nums.</param>
+        /// <param name="k">The k.</param>
+        /// <param name="expectedResult">The expected result.</param>
+        [Theory]
+        [MemberData(nameof(KSumTestData))]
+        public void KSumTest(int[] nums, int k, long expectedResult)
         {
-            LeetCodeContest307.Question4();
+            Assert.Equal(expectedResult, KSum(nums, k));
         }
 
         #endregion
--- JobSearchCodeExamples/JobSearchCodeExamples/Contest/LeetCodeContest307.cs
+++ JobSearchCodeExamples/JobSearchCodeExamples/Contest/LeetCodeContest307.cs
@@ -5,50 +5,223 @@
     /// </summary>
     public static class LeetCodeContest307
     {
-        #region Question 1 ------------------------------------------
+        #region Minimum Hours of Training to Win a Competition ------
 
         /// <summary>
         /// Question 1.
         /// </summary>
-        public static void Question1()
+        /// <param name="initialEnergy">The initial energy.</param>
+        /// <param name="initialExperience">The initial experience.</param>
+        /// <param name="energy">The energy.</param>
+        /// <param name="experience">The experience.</param>
+        /// <returns></returns>
+        public static int MinNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience)
         {
-
+            var neededEnergy = energy.Sum() + 1;
+            var trainedEnergy = Math.Max(neededEnergy - initialEnergy, 0);
+            var neededExperience = initialExperience;
+            var trainedExerience = 0;
+            if (experience != null)
+            {
+                for (int i = 0; i < experience.Length; i++)
+                    if (neededExperience > experience[i])
+                        neededExperience += experience[i];
+                    else
+                    {
+                        trainedExerience += experience[i] - neededExperience + 1;
+                        neededExperience += experience[i] - neededExperience + 1 + experience[i];
+                    }
+            }
+            return trainedEnergy + trainedExerience;
         }
 
         #endregion
 
-        #region Question 2 ------------------------------------------
+        #region Largest Palindromic Number --------------------------
 
         /// <summary>
         /// Question 2.
         /// </summary>
-        public static void Question2()
+        /// <param name="num">The number.</param>
+        /// <returns></returns>
+        public static string LargestPalindromic(string num)
         {
+            var digits = new Dictionary<int, int>();
+            string start = string.Empty;
+            string ending = string.Empty;
 
+            for (int i = 0; i < num.Length; i++)
+            {
+                var digit = Convert.ToInt32(num[i].ToString());
+                if (digits.ContainsKey(digit))
+                    digits[digit]++;
+                else
+                    digits.Add(digit, 1);
+            }
+            bool good = true;
+            int[] digitsKeys = digits.Keys.ToArray();
+            int largest;
+            var first = true;
+            while (good)
+            {
+                largest = -1;
+                for (int i = 0; i < digitsKeys.Length; i++)
+                    if (digitsKeys[i] > largest && digits[digitsKeys[i]] > 1)
+                        largest = digitsKeys[i];
+                if (largest == -1)
+                    good = false;
+                else if ((first && largest != 0) || !first)
+                {
+                    first = false;
+                    while (digits[largest] > 1)
+                    {
+                        start += largest.ToString();
+                        ending = largest.ToString() + ending.ToString();
+                        digits[largest] -= 2;
+                    }
+                }
+                else
+                    good = false;
+            }
+            largest = -1;
+            for (int i = 0; i < digitsKeys.Length; i++)
+                if (digitsKeys[i] > largest && digits[digitsKeys[i]] > 0)
+                    largest = digitsKeys[i];
+            if (largest != -1)
+                start += largest.ToString();
+
+            return start + ending;
+        }
+
+        /// <summary>
+        /// Question 2.
+        /// </summary>
+        /// <param name="num">The number.</param>
+        /// <returns></returns>
+        public static string LargestPalindromic2(string num)
+        {
+            var map = new int[10];
+            string start = string.Empty;
+
+            for (int i = 0; i < num.Length; i++)
+                map[num[i] - '0']++;
+            for (int i = 9; i > -1; i--)
+            {
+                if (i == 0 && string.IsNullOrEmpty(start)) break;
+                if (map[i] > 1)
+                {
+                    start += new string(i.ToString().ToCharArray()[0], map[i] / 2);
+                    map[i] = map[i] % 2;
+                }
+            }
+            string ending = string.Empty;
+            if (!string.IsNullOrEmpty(start))
+                ending = new string(start.Reverse().ToArray());
+            for (int i = 9; i > -1; i--)
+                if (map[i] > 0)
+                {
+                    start += i.ToString();
+                    break;
+                }
+
+            return start + ending;
         }
 
         #endregion
 
-        #region Question 3 ------------------------------------------
+        #region Amount of Time for Binary Tree to Be Infected -------
 
         /// <summary>
         /// Question 3.
         /// </summary>
-        public static void Question3()
+        public static int AmountOfTime(TreeNode root, int start)
         {
+            return 0;
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public class TreeNode
+        {
+            /// <summary>
+            /// The value
+            /// </summary>
+            public int val;
+            /// <summary>
+            /// The position
+            /// </summary>
+            public int pos;
+            /// <summary>
+            /// The left
+            /// </summary>
+            public TreeNode? left;
+            /// <summary>
+            /// The right
+            /// </summary>
+            public TreeNode? right;
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TreeNode" /> class.
+            /// </summary>
+            /// <param name="val">The value.</param>
+            /// <param name="left">The left.</param>
+            /// <param name="right">The right.</param>
+            public TreeNode(int val = 0, int pos = 0, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.val = val;
+                this.pos = pos;
+                this.left = left;
+                this.right = right;
+            }
         }
 
         #endregion
 
-        #region Question 4 ------------------------------------------
+        #region Find the K-Sum of an Array --------------------------
 
         /// <summary>
         /// Question 4.
         /// </summary>
-        public static void Question4()
+        public static long KSum(int[] nums, int k)
         {
+            Array.Sort(nums);
+            Array.Reverse(nums); // Descending Order
+            var maxSum = (from num in nums select (long)num).Sum(x => Math.Max(0, x));
+            var n = nums.Length;
+            var sums = new long[] { };
+            AddToArray(ref sums, maxSum);
+            for (var i = 0; i < n; i++)
+            {
+                var m = sums.Length;
+                var numValue = Math.Abs(nums[i]);
+                if (m >= k && maxSum - numValue < sums[m - 1]) break;
+                for (var j = 0; j < m; j++)
+                {
+                    var value = sums[j] - numValue;
+                    if (m <= k || value > sums[m - 1])
+                        AddToArray(ref sums, value);
+                    else
+                        break;
+                }
+                if (sums.Length > k)
+                {
+                    Array.Sort(sums);
+                    Array.Reverse(sums);
+                    Array.Resize(ref sums, k);
+                }
+            }
+            while (sums.Length < k)
+                AddToArray(ref sums, 0);
+            Array.Sort(sums);
+            Array.Reverse(sums);
+            return sums[^1];
+        }
 
+        private static void AddToArray(ref long[] a, long item)
+        {
+            a ??= Array.Empty<long>();
+            Array.Resize(ref a, a.Length + 1);
+            a[^1] = item;
         }
 
         #endregion
